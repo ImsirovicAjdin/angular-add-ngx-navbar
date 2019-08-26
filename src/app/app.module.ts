ce25b62a041ce0d45e9f3b6a5a97dd72ab02dea8
ImsirovicAjdin <ajdika@live.com>
2019-08-26 14:06:34 +0200

    Add the toggle functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavbarComponent } from './navigation/navigation.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    BrowserAnimationsModule, 
+    CollapseModule.forRoot() 
+  ],  
   declarations: [ AppComponent, HelloComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'nav-bar',
   templateUrl: './navigation.component.html'
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  isCollapsed = true;
+
+  toggleCollapsed() {
+    this.isCollapsed = !this.isCollapsed;
+    console.log(this.isCollapsed);
+  }  
+}
